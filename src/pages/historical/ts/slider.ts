7e89833588dddbf445a5f42c70f0b89ae8bb39b9
ligalaiz <slavakuz88@gmail.com>
2023-01-17 20:51:32 +0300

    fix: slider animation

--- src/pages/historical/ts/slider.ts
+++ src/pages/historical/ts/slider.ts
@@ -2,41 +2,56 @@ const slides: NodeListOf<Element> = document.querySelectorAll('.slider__item');
 const sliderPrevBtn: HTMLElement | null = document.getElementById('sliderPrevBtn');
 const sliderNextBtn: HTMLElement | null = document.getElementById('sliderNextBtn');
 
+let isEnabled: boolean = true;
 let currentSlideIndex = 0;
 const slidesCount = slides.length;
 
-type CurrentIndextype = (val: number) => number;
+type CurrentIndexType = (val: number) => void;
 
-const calcCurrentIndex: CurrentIndextype = (val) => val % slidesCount;
+const calcCurrentIndex: CurrentIndexType = (val): void => {
+  currentSlideIndex = (val + slidesCount) % slidesCount;
+};
 
-function directionTo(direction: 'to-left' | 'to-right'): void {
+function hideItem(direction: 'to-left' | 'to-right'): void {
+  isEnabled = false;
   slides[currentSlideIndex]?.classList.add(direction);
 
-  slides[currentSlideIndex]?.addEventListener('Animation', (): void => {
-    slides[currentSlideIndex]?.classList.remove(direction, 'active');
+  slides[currentSlideIndex]?.addEventListener('animationend', (e) => {
+    const target = e.target as HTMLElement;
+
+    target?.classList.remove(direction, 'active');
   });
 }
 
-function directionFrom(direction: 'from-left' | 'from-right'): void {
-  slides[currentSlideIndex]?.classList.add(direction);
+function showItem(direction: 'from-left' | 'from-right'): void {
+  console.log(direction)
+  slides[currentSlideIndex]?.classList.add(direction, 'next');
 
-  slides[currentSlideIndex]?.addEventListener('Animation', (): void => {
-    slides[currentSlideIndex]?.classList.remove(direction);
-    slides[currentSlideIndex]?.classList.add('active');
+  slides[currentSlideIndex]?.addEventListener('animationend', (e) => {
+    const target = e.target as HTMLElement;
+
+    target?.classList.remove(direction, 'next');
+    target?.classList.add('active');
+    isEnabled = true;
   });
 }
 
 function handleNextClick(): void {
-  directionTo('to-left');
-  currentSlideIndex = calcCurrentIndex(currentSlideIndex + 1);
-  directionFrom('from-left');
+  hideItem('to-left');
+  calcCurrentIndex(currentSlideIndex + 1);
+  showItem('from-right');
 }
 
 function handlePrevClick(): void {
-  directionTo('to-right');
-  currentSlideIndex = calcCurrentIndex(currentSlideIndex - 1);
-  directionFrom('from-right');
+  hideItem('to-right');
+  calcCurrentIndex(currentSlideIndex - 1);
+  showItem('from-left');
 }
 
-sliderNextBtn?.addEventListener('click', handleNextClick);
-sliderPrevBtn?.addEventListener('click', handlePrevClick);
+sliderNextBtn?.addEventListener('click', () => {
+  if (isEnabled) handleNextClick();
+});
+
+sliderPrevBtn?.addEventListener('click', () => {
+  if (isEnabled) handlePrevClick();
+});
